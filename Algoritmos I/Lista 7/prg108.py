0c845828d41031c6f0da5bdd61d88f38f9cb8c16
Nogit28 <99282899+Nogit28@users.noreply.github.com>
2025-08-04 20:23:10 -0300

    Add files via upload

---
+++ Algoritmos I/Lista 7/prg108.py
@@ -0,0 +1,19 @@
+'''
+prg108 - Crie um programa que gera 10 números aleatórios (entre 1 e 100) usando randint e os 
+salva  em  um  arquivo  numeros.txt  (um  por  linha).  Depois,  leia  o  arquivo  e  exiba  a  soma  dos 
+números. 
+'''
+from random import randint
+
+arquivo = open("numeros.txt","w")
+for x in range(10):
+  num = randint(1,100)
+  arquivo.write(str(num)+"\n")
+arquivo.close()
+arquivo = open("numeros.txt","r")
+soma = 0
+l = arquivo.readline()
+for l in arquivo:
+  soma += int(l)
+print("Soma:", soma)
+arquivo.close()
